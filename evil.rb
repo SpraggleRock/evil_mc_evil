e76c9471d387b7570859fc848c40f86ef5229be1
Kori Roys <kori@koriroys.com>
2014-03-24 08:59:22 -0500

    adding evil henchmen table

--- evil.rb
+++ evil.rb
@@ -13,25 +13,17 @@ ActiveSupport::LogSubscriber.colorize_logging = false
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table :users do |t|
+  create_table :evil_henchmen do |t|
     t.string :name
+    t.string :title
   end
 
-  create_table :posts do |t|
-    t.string :name
-    t.integer :user_id
-  end
 end
 
-class User < ActiveRecord::Base
-  has_many :posts
+class EvilHenchmen < ActiveRecord::Base
+
 end
 
-class Post < ActiveRecord::Base
-  belongs_to :user
-end
-
-user = User.create! name: "Josh"
-user.posts = [Post.new(name: "yo ho ho"), Post.new(name: "and a bottle of rum")]
+sauron = EvilHenchmen.create! name: "Sauron", title: "Dark Lord"
 
 binding.pry
