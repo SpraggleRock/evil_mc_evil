b573bb54cc2672eda4ad07a224e92b60cdf36759
Kori Roys <kori@koriroys.com>
2014-03-24 08:49:50 -0500

    basic in-memory database

--- evil.rb
+++ evil.rb
@@ -2,3 +2,33 @@
 # an in-memory database
 # s_arb[TAB]
 
+require 'active_record'
+require 'logger'
+
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+ActiveRecord::Base.logger = Logger.new $stdout
+ActiveSupport::LogSubscriber.colorize_logging = false
+
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :users do |t|
+    t.string :name
+  end
+
+  create_table :posts do |t|
+    t.string :name
+    t.integer :user_id
+  end
+end
+
+class User < ActiveRecord::Base
+  has_many :posts
+end
+
+class Post < ActiveRecord::Base
+  belongs_to :user
+end
+
+user = User.create! name: 'Josh'
+user.posts = [Post.new(name: 'yo ho ho'), Post.new(name: 'and a bottle of rum')]
